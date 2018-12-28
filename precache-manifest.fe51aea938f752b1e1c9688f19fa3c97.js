39f78a6707df5bdcbc54c7b073738fcbd85321bd
Andrii Osipov <a.osipov2@samsung.local>
2018-12-28 17:08:16 +0100

    Updates

---
+++ precache-manifest.fe51aea938f752b1e1c9688f19fa3c97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c920b1492c1f7473064",
+    "url": "/React-Redux/static/js/runtime~main.4c920b14.js"
+  },
+  {
+    "revision": "50684c1f25765420e784",
+    "url": "/React-Redux/static/js/main.50684c1f.chunk.js"
+  },
+  {
+    "revision": "86fd2c6431b26deabcf5",
+    "url": "/React-Redux/static/js/1.86fd2c64.chunk.js"
+  },
+  {
+    "revision": "50684c1f25765420e784",
+    "url": "/React-Redux/static/css/main.c745e23c.chunk.css"
+  },
+  {
+    "revision": "83237cff09a46fe8f2b08e8a687a4533",
+    "url": "/React-Redux/index.html"
+  }
+];
