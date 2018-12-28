1e906ae64b7501f45ada1f2d63f9e2fb2c262486
Andrii Osipov <a.osipov2@samsung.local>
2018-12-28 12:04:36 +0100

    Updated folder structure

--- src/components/search-panel.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './search-panel.css';
-
-const SearchPanel = () => {
-    return <input placeholder="search" className="search-input" />
-}
-
-export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list'
 
-const App = () => {
+import App from './components/app/app'
 
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make an App', important: true, id: 2 },
-        { label: 'Have a Lunch', important: false, id: 3 },
-    ];
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList todos={todoData} />
-        </div>
-
-    )
-}
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />,
+  document.getElementById('root'));
